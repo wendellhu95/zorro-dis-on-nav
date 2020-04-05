2b452666c41e8ca1867f5ea59780481c32f63705
Wendell Hu <wendellhu95@gmail.com>
2020-04-05 18:51:51 +0800

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dis-on-nav';
+  name = 'Angular';
+}
+
+@Component({
+  selector: 'app-child',
+  template: `
+    <span nz-tooltip [nzTooltipTitle]="nzTemplate"
+      >Tooltip will show when mouse enter.</span
+    >
+    <ng-template #nzTemplate>
+      <i class="anticon anticon-file"></i> <a href="#/test">test</a>
+    </ng-template>
+
+    <input nz-input [(ngModel)]="value" [nzAutocomplete]="auto" />
+    <nz-autocomplete
+      [nzDataSource]="['12345', '23456', '34567']"
+      #auto
+    ></nz-autocomplete>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class ChildComponent implements AfterViewInit {
+  name = 'Angular';
+
+  value = '12345';
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.router.navigate(['test']);
+    }, 3000);
+  }
+}
+
+@Component({
+  selector: "my-test",
+  template: `test`,
+  styleUrls: ['./app.component.css'],
+})
+export class TestComponent {
+  name = 'Angular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, RouteReuseStrategy } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppComponent, ChildComponent, TestComponent } from './app.component';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NZ_I18N, en_US } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { SimpleReuseStrategy } from './reuse';
+registerLocaleData(en);
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    NgZorroAntdModule,
+    RouterModule.forRoot(
+      [
+        { path: '', pathMatch: 'full', redirectTo: '/demo' },
+        { path: 'demo', component: ChildComponent },
+        { path: 'test', component: TestComponent },
+      ],
+      { useHash: true }
+    ),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ChildComponent, TestComponent],
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    { provide: RouteReuseStrategy, useClass: SimpleReuseStrategy },
+  ],
 })
-export class AppModule { }
+export class AppModule {}
